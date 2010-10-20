111382482eb04c07aeb07300cb3eee5b3af15d87
nevasion <nevasion@gmail.com>
2010-10-20 17:41:55 +0200

    added command relay logic

---
+++ src/GandiDesktop/ViewModel/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GandiDesktop.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (this.canExecute == null ? true : this.canExecute(parameter));
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
