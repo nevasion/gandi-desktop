4a3359bfc6d8bb7c1b2dd3709fddaf5492e3942b
nevasion <nevasion@gmail.com>
2010-10-26 19:07:37 +0200

    services: updated method names to consts

--- src/GandiDesktop.Gandi.Services/Hosting/IHostingService.cs
+++ src/GandiDesktop.Gandi.Services/Hosting/IHostingService.cs
@@ -2,67 +2,106 @@
 
 namespace GandiDesktop.Gandi.Services.Hosting
 {
-    [XmlRpcUrl("https://rpc.gandi.net/xmlrpc/2.0/")]
+    static class XmlRpcUrl
+    {
+        public const string Url = "https://rpc.gandi.net/xmlrpc/2.0/";
+    }
+
+    static class XmlRpcMethodNames
+    {
+        public const string DataCenterList = "datacenter.list";
+        public const string DiskList = "disk.list";
+        public const string DiskCreate = "disk.create";
+        public const string DiskCreateFrom = "disk.create_from";
+        public const string DiskUpdate = "disk.update";
+        public const string DiskDelete = "disk.delete";
+        public const string InterfaceList = "iface.list";
+        public const string InterfaceCreate = "iface.create";
+        public const string InterfaceUpdate = "iface.update";
+        public const string InterfaceDelete = "iface.delete";
+        public const string InterfaceAttachIp = "iface.ip_attach";
+        public const string InterfaceDetachIp = "iface.ip_detach";
+        public const string ImageList = "image.list";
+        public const string IpAddressList = "ip.list";
+        public const string IpAddressCreate = "ip.create";
+        public const string IpAddressUpdate = "ip.update";
+        public const string IpAddressDelete = "ip.delete";
+        public const string VirtualMachineList = "vm.list";
+        public const string VirtualMachineCreate = "vm.create";
+        public const string VirtualMachineCreateFrom = "vm.create_from";
+        public const string VirtualMachineUpdate = "vm.update";
+        public const string VirtualMachineDelete = "vm.delete";
+        public const string VirtualMachineStart = "vm.start";
+        public const string VirtualMachineStop = "vm.stop";
+    }
+
+    [XmlRpcUrl(XmlRpcUrl.Url)]
     public interface IHostingService : IXmlRpcProxy
     {
-        [XmlRpcMethod("datacenter.list")]
+        #region DataCenter
+        [XmlRpcMethod(XmlRpcMethodNames.DataCenterList)]
         Mapping.DataCenter[] datacenter_list(string apiKey);
+        #endregion
 
         #region Disk
-        [XmlRpcMethod("disk.list")]
+        [XmlRpcMethod(XmlRpcMethodNames.DiskList)]
         Mapping.Disk[] disk_list(string apiKey);
 
-        [XmlRpcMethod("disk.create")]
+        [XmlRpcMethod(XmlRpcMethodNames.DiskCreate)]
         object disk_create(string apiKey, Mapping.DiskCreation diskCreation);
 
-        [XmlRpcMethod("disk.create_from")]
+        [XmlRpcMethod(XmlRpcMethodNames.DiskCreateFrom)]
         object disk_create_from(string apiKey, Mapping.DiskCopy diskCopy, int sourceDiskId);
 
-        [XmlRpcMethod("disk.update")]
+        [XmlRpcMethod(XmlRpcMethodNames.DiskUpdate)]
         object disk_update(string apiKey, int diskId, Mapping.DiskUpdate diskUpdate);
 
-        [XmlRpcMethod("disk.delete")]
+        [XmlRpcMethod(XmlRpcMethodNames.DiskDelete)]
         object disk_delete(string apiKey, int diskId);
         #endregion
 
         #region Interface
-        [XmlRpcMethod("iface.list")]
+        [XmlRpcMethod(XmlRpcMethodNames.InterfaceList)]
         Mapping.Interface[] iface_list(string apiKey);
 
-        [XmlRpcMethod("iface.create")]
+        [XmlRpcMethod(XmlRpcMethodNames.InterfaceCreate)]
         object iface_create(string apiKey, Mapping.InterfaceCreation ifaceCreation);
 
-        [XmlRpcMethod("iface.update")]
+        [XmlRpcMethod(XmlRpcMethodNames.InterfaceUpdate)]
         object iface_update(string apiKey, int ifaceId, Mapping.InterfaceUpdate ifaceUpdate);
 
-        [XmlRpcMethod("iface.delete")]
+        [XmlRpcMethod(XmlRpcMethodNames.InterfaceDelete)]
         object iface_delete(string apiKey, int ifaceId);
 
-        [XmlRpcMethod("iface.ip_attach")]
+        [XmlRpcMethod(XmlRpcMethodNames.InterfaceAttachIp)]
         object iface_ip_attach(string apiKey, int ifaceId, int ipAddressId);
 
-        [XmlRpcMethod("iface.ip_detach")]
+        [XmlRpcMethod(XmlRpcMethodNames.InterfaceDetachIp)]
         object iface_ip_detach(string apiKey, int ifaceId, int ipAddressId);
         #endregion
 
-        [XmlRpcMethod("image.list")]
+        #region Image
+        [XmlRpcMethod(XmlRpcMethodNames.ImageList)]
         Mapping.Image[] image_list(string apiKey);
+        #endregion
 
         #region IpAddress
-        [XmlRpcMethod("ip.list")]
+        [XmlRpcMethod(XmlRpcMethodNames.IpAddressList)]
         Mapping.IpAddress[] ip_list(string apiKey);
 
-        [XmlRpcMethod("ip.create")]
+        [XmlRpcMethod(XmlRpcMethodNames.IpAddressCreate)]
         object ip_create(string apiKey, Mapping.IpAddressCreation ipAddressCreation);
 
-        [XmlRpcMethod("ip.update")]
+        [XmlRpcMethod(XmlRpcMethodNames.IpAddressUpdate)]
         object ip_update(string apiKey, int ipAddressId, Mapping.IpAddressUpdate ipAddressUpdate);
 
-        [XmlRpcMethod("ip.delete")]
+        [XmlRpcMethod(XmlRpcMethodNames.IpAddressDelete)]
         object ip_delete(string apiKey, int ipAddressId);
         #endregion
 
-        [XmlRpcMethod("vm.list")]
+        #region VirtualMachine
+        [XmlRpcMethod(XmlRpcMethodNames.VirtualMachineList)]
         Mapping.VirtualMachine[] vm_list(string apiKey);
+        #endregion
     }
 }
